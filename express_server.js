91fda8875898ae609755cbb1ba5ab14e1865d2a2
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-24 13:59:04 -0500

    Add EJS

--- express_server.js
+++ express_server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -22,4 +24,3 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
