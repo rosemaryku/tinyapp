760d5e6b08bca424adbdf42e40b3920b41c1ff33
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-24 14:40:06 -0500

    Add urls_index ejs file

--- express_server.js
+++ express_server.js
@@ -5,12 +5,17 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
